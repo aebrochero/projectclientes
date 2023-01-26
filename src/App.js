cc3bd2ef51ec43b51b966c61e5aeeef971cf1049
aebrochero <aebrochero@utp.edu.co>
2023-01-26 18:03:29 -0500

    Setting-Div

--- src/App.js
+++ src/App.js
@@ -4,18 +4,12 @@ import './App.css';
 
 function App() {
   return (
-          <div className='flex flex-col space-y-10 space-x-5 bg-green-300'> 
-
-    <h1 className="text-orange-800 text-3xl font-bold underline">
-        
-          Hello world!
-        </h1>
-        <h2>
-          Dick-blower
-        </h2>
+          <div className='flex flex-col space-y-10 space-x-5 bg-gray-300 w-96 h-96' > 
+          
           </div>
-      
-    
+          
+          
+            
   );
 }
 
