092dd990b03502a0462fe0f276db704c2dbb6b56
aebrochero <aebrochero@utp.edu.co>
2023-01-25 23:55:42 -0500

    tailwind full config

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
-    
-        <h1 className="text-orange-200 text-3xl font-bold underline">
+          <div className='flex flex-col space-y-10 space-x-5 bg-green-300'> 
+
+    <h1 className="text-orange-800 text-3xl font-bold underline">
+        
           Hello world!
         </h1>
+        <h2>
+          Dick-blower
+        </h2>
+          </div>
       
     
   );
