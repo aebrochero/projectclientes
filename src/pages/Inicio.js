8d56c2ca78e5ccba97f4d88ea0beec816606e574
aebrochero <aebrochero@utp.edu.co>
2023-01-29 11:21:32 -0500

    fixed submit form

--- src/pages/Inicio.js
+++ src/pages/Inicio.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div className='w-20 h-20 bg-purple-600'>Hello World sopla mondas!!!!!!!!!</div>
+    <div className='w-20 h-20 bg-purple-600'>"Lorem ipsum dolor sit amet, consectetur adipiscing elit"</div>
   )
 }
 
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -8,23 +8,24 @@ const Login = () => {
 
   const handleButtonForgot = () => {
     alert("Se ha enviado un link a tu correo")
+    navigate("/")
   }
 
-  const handleOnSubmit = () => {
+  const handleOnClick = () => {
     alert("Bienvenido a la familia Servitel")
     navigate("/inicio")
   }
 
   return (
     <section className='w-full h-screen flex justify-center'>
-        <div className='max-w-[1100px] h-full flex'>
-            <div className='w-1/2 h-full flex flex-col justify-center space-y-4 px-3'>
+        <div className='max-w-[1080px] h-full flex'>
+            <div className='w-1/2 h-full flex flex-col justify-center space-y-4 px-10'>
                 <img src="https://servitel.co/wp-content/uploads/2019/04/Mesa-de-trabajo-1@280x-8.png" alt="" />
                 <div>
-                    <h1 className='text-orange-500 text-2xl font-semibold'>Sign In</h1>
-                    <p className='text-gray-400'>Lorem Iptum</p>
+                    <h1 className='text-orange-500 text-2xl font-semibold'>Inicio de sesión</h1>
+                    <p className='text-gray-400'>Por Favor introduzca sus datos de usuario</p>
                 </div>
-                <form onSubmit={handleOnSubmit} className='flex flex-col space-y-3' action="">
+                <form className='flex flex-col space-y-3' action="">
                     <div className='flex flex-col space-y-2'>
                         <input className='w-full h-10 border-input bg-white rounded-xl border-2 border-gray-500' type="text" placeholder='Username'/>
                         <input className='w-full h-10 border-input bg-white rounded-xl border-2 border-gray-500' type="password" placeholder='Password'/>
@@ -32,11 +33,11 @@ const Login = () => {
                     <div className='w-full flex justify-between text-gray-600'>
                         <div className='flex'>
                             <input className='pr-2' type="checkbox" />
-                            Remember me
+                            Recordar Usuario
                         </div>
-                        <button onClick={handleButtonForgot}>Forgot my password</button>
+                        <button onClick={handleButtonForgot}>Olvidé mi contraseña</button>
                     </div>
-                    <button className='w-full h-12 bg-blue-700 text-white rounded-xl'>Log in</button>
+                    <button onClick={handleOnClick} className='w-full h-12 bg-blue-700 text-white rounded-xl'>Log in</button>
                 </form>
             </div>
             <div className='h-[500px]'>
